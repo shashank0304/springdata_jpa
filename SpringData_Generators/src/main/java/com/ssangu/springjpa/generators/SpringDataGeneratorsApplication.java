1bd0d9ba00e5064679f77e1dac496ef7d6b68fb9
shashank0304 <shashank0304@gmail.com>
2022-02-01 20:43:19 +0530

    Initial Commit for Spring Data JPA Generators

---
+++ SpringData_Generators/src/main/java/com/ssangu/springjpa/generators/SpringDataGeneratorsApplication.java
@@ -0,0 +1,13 @@
+package com.ssangu.springjpa.generators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataGeneratorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataGeneratorsApplication.class, args);
+	}
+
+}
---
+++ SpringData_Generators/src/test/java/com/ssangu/springjpa/generators/SpringDataGeneratorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssangu.springjpa.generators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataGeneratorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
